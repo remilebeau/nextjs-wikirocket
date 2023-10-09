127176c2f983c17031fe041ddfce14b669743b49
RemiLeBeau <remilebeau90@gmail.com>
2023-10-09 15:13:39 -0400

    Added instructions to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl dark:text-white font-bold">
+        Enter a Wikipedia search term in the field above
+      </h1>
+    </main>
   );
 }
