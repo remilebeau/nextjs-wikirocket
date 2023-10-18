0a95b054935f7bce3391883ddefa14c1c74845bf
RemiLeBeau <no@thanks.com>
2023-10-18 00:51:41 -0400

    fixed the map function inside SearchResults()

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -31,9 +31,9 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const content = (
     <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
-        Object.values(results).map((result) => (
-          <p>{<Item key={result.pageid} result={result} />}</p>
-        ))
+        Object.values(results).map((result) => {
+          return <Item key={result.pageid} result={result} />;
+        })
       ) : (
         <h2 className="p-2 text-xl">{`${searchTerm} not found`}</h2>
       )}
